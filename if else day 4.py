2b4d8560249cfe29d19cef057aef1749b5ad3d59
Gaurav Bele <gauravbele001@gmail.com>
2026-01-12 15:54:36 +0530

    Add files via upload

---
+++ if else day 4.py
@@ -0,0 +1,9 @@
+print("conditions and loops")
+
+age = int(input("enter age : "))
+
+if age >= 18:
+    print("eligible for voting in India")
+else:
+    print("not eligible")
+    
---
+++ if–elif–else.py
@@ -0,0 +1,12 @@
+print("if–elif–else demo program")
+
+marks = int(input("enter marks: "))
+
+if marks >= 75:
+    print("Distictions")
+elif marks >= 60:
+    print("First Class")
+elif marks >= 40:
+    print("pass")
+else:
+    print("fail")
